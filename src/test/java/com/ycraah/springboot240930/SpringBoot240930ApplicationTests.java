85ee0763755d5f3eb0702647692a0f5dd508b33a
김예찬 <ycraah@gamil.com>
2024-09-30 19:41:41 +0900

    ãcoffee 객체 새생성

---
+++ src/test/java/com/ycraah/springboot240930/SpringBoot240930ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycraah.springboot240930;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot240930ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
